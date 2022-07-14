69820d0c193e7d99e1c567b9f117fd80d37624e4
SKYYgraphtools <109234723+SKYYgraphtools@users.noreply.github.com>
2022-07-14 13:51:41 +0200

    Add files via upload

---
+++ ddos.tools/ping.py
@@ -0,0 +1,46 @@
+from colorama import Back, Fore, Style, deinit, init
+import time 
+init(convert=True)
+print(Fore.LIGHTBLUE_EX + """ 
+                         ███████╗██╗  ██╗██╗   ██╗██╗   ██╗
+                         ██╔════╝██║ ██╔╝╚██╗ ██╔╝╚██╗ ██╔╝
+                         ███████╗█████╔╝  ╚████╔╝  ╚████╔╝ 
+                         ╚════██║██╔═██╗   ╚██╔╝    ╚██╔╝  
+                         ███████║██║  ██╗   ██║      ██║   
+                         ╚══════╝╚═╝  ╚═╝   ╚═╝      ╚═╝ 
+                     ══╦═════════════════════════════════╦══
+             ╔═════════╩═════════════════════════════════╩═════════╗
+             ║         Welcome To ping tools of skyy               ║
+             ║                  ENJOI                              ║
+             ║             discord : skyynet#0001                  ║
+             ╚═════════════════════════════════════════════════════╝ """)
+
+
+ipp = input("l'ip a ping : ")
+for i in range(0,15): 
+    i ="Réponse de " + ipp + ": octets=32 temps=16 ms TTL=56"
+  
+    print(i)
+    
+  
+    time.sleep(0.5)
+
+
+
+
+for i in range(0,15): 
+    i ="Réponse de " + ipp + ": octets=32 temps=1800 ms TTL=56"
+  
+    print(i)
+    
+  
+    time.sleep(0.5)
+
+print("cible down vous pouvez stopp le ddos")
+
+
+
+
+
+
+
